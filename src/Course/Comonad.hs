6935adf7c441a248751b0c43c1b2e8469aaa22d2
Logan Lowder <logan.lowder@logikcull.com>
2020-12-28 23:17:12 -0500

    Complete comonad

--- src/Course/Comonad.hs
+++ src/Course/Comonad.hs
@@ -1,6 +1,6 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Course.Comonad where
 
@@ -17,29 +17,19 @@ import Course.Extend
 -- * The law of right identity
 --   `∀f. copure . (f <<=) == f
 class Extend k => Comonad k where
-  copure ::
-    k a
-    -> a
+  copure :: k a -> a
 
 -- | Implement the @Comonad@ instance for @ExactlyOne@.
 --
 -- >>> copure (ExactlyOne 7)
 -- 7
 instance Comonad ExactlyOne where
-  copure ::
-    ExactlyOne a
-    -> a
-  copure =
-    error "todo: Course.Comonad copure#instance ExactlyOne"
+  copure :: ExactlyOne a -> a
+  copure = runExactlyOne
 
 -- | Witness that all things with (<<=) and copure also have (<$>).
 --
 -- >>> (+10) <$$> ExactlyOne 7
 -- ExactlyOne 17
-(<$$>) ::
-  Comonad k =>
-  (a -> b)
-  -> k a
-  -> k b
-(<$$>) =
-  error "todo: Course.Comonad#(<$>)"
+(<$$>) :: Comonad k => (a -> b) -> k a -> k b
+(<$$>) f = (f . copure <<=)
