796cee51e2912085224f99337595226c5b5c325d
Logan Lowder <logan.lowder@logikcull.com>
2021-01-01 14:49:22 -0600

    Complete Compose.hs

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -16,22 +16,39 @@ newtype Compose f g a = Compose (f (g a))
 
 -- Implement a Functor instance for Compose
 instance (Functor f, Functor g) => Functor (Compose f g) where
-  (<$>) = error "todo: Course.Compose (<$>)#instance (Compose f g)"
+  -- (<$>) :: (a -> b) -> k a -> k b
+  (<$>) h (Compose fg) = Compose ((h <$>) <$> fg)
 
 instance (Applicative f, Applicative g) => Applicative (Compose f g) where
   -- Implement the pure function for an Applicative instance for Compose
-  pure = error "todo: Course.Compose pure#instance (Compose f g)"
+  -- pure :: a -> k a
+  pure = Compose . pure . pure
 
   -- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) = error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  -- (<*>) :: k (a -> b) -> k a -> k b
+  (<*>) (Compose f) (Compose x) = Compose (lift2 (<*>) f x)
 
 instance (Monad f, Monad g) => Monad (Compose f g) where
   -- Implement the (=<<) function for a Monad instance for Compose
-  (=<<) = error "todo: Course.Compose (=<<)#instance (Compose f g)"
+  -- (=<<) :: (a -> k b) -> k a -> k b
+  (=<<) = error "impossible"
+
+-- There seems to be no way to join this `f (Compose f g b)`
+-- (=<<) f (Compose fg) = (f <$>) =<< fg
 
 -- Note that the inner g is Contravariant but the outer f is
 -- Functor. We would not be able to write an instance if both were
 -- Contravariant; why not?
+
+-- Answer: (>$<) needs a `ga` but we have a `gb`
+-- instance (Contravariant f, Contravariant g) => Contravariant (Compose f g) where
+--   (>$<) :: (b -> a) -> k a -> k b
+--   (>$<) ba (Compose fga) = Compose ((\gb -> _t) >$< fga)
+
+-- I also tried for other combinations of Functor and Contravariant and
+-- the types never line up.
+
 instance (Functor f, Contravariant g) => Contravariant (Compose f g) where
   -- Implement the (>$<) function for a Contravariant instance for Compose
-  (>$<) = error "todo: Course.Compose (>$<)#instance (Compose f g)"
+  -- (>$<) :: (b -> a) -> k a -> k b
+  (>$<) ba (Compose fga) = Compose ((ba >$<) <$> fga)
