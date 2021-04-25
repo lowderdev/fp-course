d233e68a87cb2f667a9512a596225ad0a8e81139
Logan Lowder <loganlowder@gmail.com>
2021-04-24 19:57:15 -0500

    First draft of Cheque

--- src/Course/Cheque.hs
+++ src/Course/Cheque.hs
@@ -1,6 +1,6 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 {-
 
@@ -19,165 +19,164 @@ data structures that may assist you in deriving the result. It is not compulsory
 
 module Course.Cheque where
 
-import Course.Core
-import Course.Optional
-import Course.List
-import Course.Functor
 import Course.Applicative
+import Course.Core
+import Course.Functor
+import Course.List
 import Course.Monad
+import Course.Optional
+import Course.Traversable
 
 -- $setup
 -- >>> :set -XOverloadedStrings
 
 -- The representation of the grouping of each exponent of one thousand. ["thousand", "million", ...]
-illion ::
-  List Chars
+illion :: List Chars
 illion =
-  let preillion ::
-        List (Chars -> Chars)
+  let preillion :: List (Chars -> Chars)
       preillion =
-        listh [
-          const ""
-        , const "un"
-        , const "do"
-        , const "tre"
-        , const "quattuor"
-        , const "quin"
-        , const "sex"
-        , const "septen"
-        , const "octo"
-        , \q -> if "n" `isPrefixOf` q then "novem" else "noven"
-        ]
-      postillion ::
-        List Chars
+        listh
+          [ const "",
+            const "un",
+            const "do",
+            const "tre",
+            const "quattuor",
+            const "quin",
+            const "sex",
+            const "septen",
+            const "octo",
+            \q -> if "n" `isPrefixOf` q then "novem" else "noven"
+          ]
+      postillion :: List Chars
       postillion =
-        listh [
-          "vigintillion"
-        , "trigintillion"
-        , "quadragintillion"
-        , "quinquagintillion"
-        , "sexagintillion"
-        , "septuagintillion"
-        , "octogintillion"
-        , "nonagintillion"
-        , "centillion"
-        , "decicentillion"
-        , "viginticentillion"
-        , "trigintacentillion"
-        , "quadragintacentillion"
-        , "quinquagintacentillion"
-        , "sexagintacentillion"
-        , "septuagintacentillion"
-        , "octogintacentillion"
-        , "nonagintacentillion"
-        , "ducentillion"
-        , "deciducentillion"
-        , "vigintiducentillion"
-        , "trigintaducentillion"
-        , "quadragintaducentillion"
-        , "quinquagintaducentillion"
-        , "sexagintaducentillion"
-        , "septuagintaducentillion"
-        , "octogintaducentillion"
-        , "nonagintaducentillion"
-        , "trecentillion"
-        , "decitrecentillion"
-        , "vigintitrecentillion"
-        , "trigintatrecentillion"
-        , "quadragintatrecentillion"
-        , "quinquagintatrecentillion"
-        , "sexagintatrecentillion"
-        , "septuagintatrecentillion"
-        , "octogintatrecentillion"
-        , "nonagintatrecentillion"
-        , "quadringentillion"
-        , "deciquadringentillion"
-        , "vigintiquadringentillion"
-        , "trigintaquadringentillion"
-        , "quadragintaquadringentillion"
-        , "quinquagintaquadringentillion"
-        , "sexagintaquadringentillion"
-        , "septuagintaquadringentillion"
-        , "octogintaquadringentillion"
-        , "nonagintaquadringentillion"
-        , "quingentillion"
-        , "deciquingentillion"
-        , "vigintiquingentillion"
-        , "trigintaquingentillion"
-        , "quadragintaquingentillion"
-        , "quinquagintaquingentillion"
-        , "sexagintaquingentillion"
-        , "septuagintaquingentillion"
-        , "octogintaquingentillion"
-        , "nonagintaquingentillion"
-        , "sescentillion"
-        , "decisescentillion"
-        , "vigintisescentillion"
-        , "trigintasescentillion"
-        , "quadragintasescentillion"
-        , "quinquagintasescentillion"
-        , "sexagintasescentillion"
-        , "septuagintasescentillion"
-        , "octogintasescentillion"
-        , "nonagintasescentillion"
-        , "septingentillion"
-        , "deciseptingentillion"
-        , "vigintiseptingentillion"
-        , "trigintaseptingentillion"
-        , "quadragintaseptingentillion"
-        , "quinquagintaseptingentillion"
-        , "sexagintaseptingentillion"
-        , "septuagintaseptingentillion"
-        , "octogintaseptingentillion"
-        , "nonagintaseptingentillion"
-        , "octingentillion"
-        , "decioctingentillion"
-        , "vigintioctingentillion"
-        , "trigintaoctingentillion"
-        , "quadragintaoctingentillion"
-        , "quinquagintaoctingentillion"
-        , "sexagintaoctingentillion"
-        , "septuagintaoctingentillion"
-        , "octogintaoctingentillion"
-        , "nonagintaoctingentillion"
-        , "nongentillion"
-        , "decinongentillion"
-        , "vigintinongentillion"
-        , "trigintanongentillion"
-        , "quadragintanongentillion"
-        , "quinquagintanongentillion"
-        , "sexagintanongentillion"
-        , "septuagintanongentillion"
-        , "octogintanongentillion"
-        , "nonagintanongentillion"
+        listh
+          [ "vigintillion",
+            "trigintillion",
+            "quadragintillion",
+            "quinquagintillion",
+            "sexagintillion",
+            "septuagintillion",
+            "octogintillion",
+            "nonagintillion",
+            "centillion",
+            "decicentillion",
+            "viginticentillion",
+            "trigintacentillion",
+            "quadragintacentillion",
+            "quinquagintacentillion",
+            "sexagintacentillion",
+            "septuagintacentillion",
+            "octogintacentillion",
+            "nonagintacentillion",
+            "ducentillion",
+            "deciducentillion",
+            "vigintiducentillion",
+            "trigintaducentillion",
+            "quadragintaducentillion",
+            "quinquagintaducentillion",
+            "sexagintaducentillion",
+            "septuagintaducentillion",
+            "octogintaducentillion",
+            "nonagintaducentillion",
+            "trecentillion",
+            "decitrecentillion",
+            "vigintitrecentillion",
+            "trigintatrecentillion",
+            "quadragintatrecentillion",
+            "quinquagintatrecentillion",
+            "sexagintatrecentillion",
+            "septuagintatrecentillion",
+            "octogintatrecentillion",
+            "nonagintatrecentillion",
+            "quadringentillion",
+            "deciquadringentillion",
+            "vigintiquadringentillion",
+            "trigintaquadringentillion",
+            "quadragintaquadringentillion",
+            "quinquagintaquadringentillion",
+            "sexagintaquadringentillion",
+            "septuagintaquadringentillion",
+            "octogintaquadringentillion",
+            "nonagintaquadringentillion",
+            "quingentillion",
+            "deciquingentillion",
+            "vigintiquingentillion",
+            "trigintaquingentillion",
+            "quadragintaquingentillion",
+            "quinquagintaquingentillion",
+            "sexagintaquingentillion",
+            "septuagintaquingentillion",
+            "octogintaquingentillion",
+            "nonagintaquingentillion",
+            "sescentillion",
+            "decisescentillion",
+            "vigintisescentillion",
+            "trigintasescentillion",
+            "quadragintasescentillion",
+            "quinquagintasescentillion",
+            "sexagintasescentillion",
+            "septuagintasescentillion",
+            "octogintasescentillion",
+            "nonagintasescentillion",
+            "septingentillion",
+            "deciseptingentillion",
+            "vigintiseptingentillion",
+            "trigintaseptingentillion",
+            "quadragintaseptingentillion",
+            "quinquagintaseptingentillion",
+            "sexagintaseptingentillion",
+            "septuagintaseptingentillion",
+            "octogintaseptingentillion",
+            "nonagintaseptingentillion",
+            "octingentillion",
+            "decioctingentillion",
+            "vigintioctingentillion",
+            "trigintaoctingentillion",
+            "quadragintaoctingentillion",
+            "quinquagintaoctingentillion",
+            "sexagintaoctingentillion",
+            "septuagintaoctingentillion",
+            "octogintaoctingentillion",
+            "nonagintaoctingentillion",
+            "nongentillion",
+            "decinongentillion",
+            "vigintinongentillion",
+            "trigintanongentillion",
+            "quadragintanongentillion",
+            "quinquagintanongentillion",
+            "sexagintanongentillion",
+            "septuagintanongentillion",
+            "octogintanongentillion",
+            "nonagintanongentillion"
+          ]
+   in listh
+        [ "",
+          "thousand",
+          "million",
+          "billion",
+          "trillion",
+          "quadrillion",
+          "quintillion",
+          "sextillion",
+          "septillion",
+          "octillion",
+          "nonillion",
+          "decillion",
+          "undecillion",
+          "duodecillion",
+          "tredecillion",
+          "quattuordecillion",
+          "quindecillion",
+          "sexdecillion",
+          "septendecillion",
+          "octodecillion",
+          "novemdecillion"
         ]
-  in listh [
-       ""
-     , "thousand"
-     , "million"
-     , "billion"
-     , "trillion"
-     , "quadrillion"
-     , "quintillion"
-     , "sextillion"
-     , "septillion"
-     , "octillion"
-     , "nonillion"
-     , "decillion"
-     , "undecillion"
-     , "duodecillion"
-     , "tredecillion"
-     , "quattuordecillion"
-     , "quindecillion"
-     , "sexdecillion"
-     , "septendecillion"
-     , "octodecillion"
-     , "novemdecillion"
-     ] ++ lift2 ((++) =<<) preillion postillion
+        ++ lift2 ((++) =<<) preillion postillion
 
 -- A data type representing the digits zero to nine.
-data Digit =
-  Zero
+data Digit
+  = Zero
   | One
   | Two
   | Three
@@ -187,65 +186,68 @@ data Digit =
   | Seven
   | Eight
   | Nine
-  deriving (Eq, Ord)
+  deriving (Eq, Ord, Show)
 
-showDigit ::
-  Digit
-  -> Chars
-showDigit Zero =
-  "zero"
-showDigit One =
-  "one"
-showDigit Two =
-  "two"
-showDigit Three =
-  "three"
-showDigit Four =
-  "four"
-showDigit Five =
-  "five"
-showDigit Six =
-  "six"
-showDigit Seven =
-  "seven"
-showDigit Eight =
-  "eight"
-showDigit Nine =
-  "nine"
+showDigit :: Digit -> Chars
+showDigit Zero = "zero"
+showDigit One = "one"
+showDigit Two = "two"
+showDigit Three = "three"
+showDigit Four = "four"
+showDigit Five = "five"
+showDigit Six = "six"
+showDigit Seven = "seven"
+showDigit Eight = "eight"
+showDigit Nine = "nine"
 
 -- A data type representing one, two or three digits, which may be useful for grouping.
-data Digit3 =
-  D1 Digit
+data Digit3
+  = D1 Digit
   | D2 Digit Digit
   | D3 Digit Digit Digit
-  deriving Eq
+  deriving (Eq, Show)
+
+showDigit3 :: Digit3 -> Chars
+-- D1
+showDigit3 (D1 Zero) = ""
+showDigit3 (D1 d) = showDigit d
+-- D2
+showDigit3 (D2 Zero c) = showDigit3 (D1 c)
+showDigit3 (D2 One Zero) = "ten"
+showDigit3 (D2 One One) = "eleven"
+showDigit3 (D2 One Two) = "twelve"
+showDigit3 (D2 One Three) = "thirteen"
+showDigit3 (D2 One Four) = "fourteen"
+showDigit3 (D2 One Five) = "fifteen"
+showDigit3 (D2 One Six) = "sixteen"
+showDigit3 (D2 One Seven) = "seventeen"
+showDigit3 (D2 One Eight) = "eighteen"
+showDigit3 (D2 One Nine) = "nineteen"
+showDigit3 (D2 Two c) = "twenty-" ++ showDigit c
+showDigit3 (D2 Three c) = "thirty-" ++ showDigit c
+showDigit3 (D2 Four c) = "forty-" ++ showDigit c
+showDigit3 (D2 Five c) = "fifty-" ++ showDigit c
+showDigit3 (D2 Six c) = "sixty-" ++ showDigit c
+showDigit3 (D2 Seven c) = "seventy-" ++ showDigit c
+showDigit3 (D2 Eight c) = "eighty-" ++ showDigit c
+showDigit3 (D2 Nine c) = "ninety-" ++ showDigit c
+-- D3
+showDigit3 (D3 Zero b c) = showDigit3 (D2 b c)
+showDigit3 (D3 a b c) = showDigit a ++ " hundred " ++ showDigit3 (D2 b c)
 
 -- Possibly convert a character to a digit.
-fromChar ::
-  Char
-  -> Optional Digit
-fromChar '0' =
-  Full Zero
-fromChar '1' =
-  Full One
-fromChar '2' =
-  Full Two
-fromChar '3' =
-  Full Three
-fromChar '4' =
-  Full Four
-fromChar '5' =
-  Full Five
-fromChar '6' =
-  Full Six
-fromChar '7' =
-  Full Seven
-fromChar '8' =
-  Full Eight
-fromChar '9' =
-  Full Nine
-fromChar _ =
-  Empty
+fromChar :: Char -> Optional Digit
+fromChar '0' = Full Zero
+fromChar '1' = Full One
+fromChar '2' = Full Two
+fromChar '3' = Full Three
+fromChar '4' = Full Four
+fromChar '5' = Full Five
+fromChar '6' = Full Six
+fromChar '7' = Full Seven
+fromChar '8' = Full Eight
+fromChar '9' = Full Nine
+fromChar _ = Empty
 
 -- | Take a numeric value and produce its English output.
 --
@@ -320,8 +322,54 @@ fromChar _ =
 --
 -- >>> dollars "456789123456789012345678901234567890123456789012345678901234567890.12"
 -- "four hundred and fifty-six vigintillion seven hundred and eighty-nine novemdecillion one hundred and twenty-three octodecillion four hundred and fifty-six septendecillion seven hundred and eighty-nine sexdecillion twelve quindecillion three hundred and forty-five quattuordecillion six hundred and seventy-eight tredecillion nine hundred and one duodecillion two hundred and thirty-four undecillion five hundred and sixty-seven decillion eight hundred and ninety nonillion one hundred and twenty-three octillion four hundred and fifty-six septillion seven hundred and eighty-nine sextillion twelve quintillion three hundred and forty-five quadrillion six hundred and seventy-eight trillion nine hundred and one billion two hundred and thirty-four million five hundred and sixty-seven thousand eight hundred and ninety dollars and twelve cents"
-dollars ::
-  Chars
-  -> Chars
-dollars =
-  error "todo: Course.Cheque#dollars"
+dollars :: Chars -> Chars
+dollars s =
+  let filtered = filter (\c -> isDigit c || (c == '.')) s
+      (preDecimal, decimal) = break (== '.') filtered
+      postDecimal = drop 1 decimal
+   in translate (groupings preDecimal)
+        ++ "dollars"
+        ++ " and "
+        ++ translate (groupings postDecimal)
+        ++ "cents"
+
+groupings :: Chars -> List (Digit3, Chars)
+groupings s =
+  let reversedGroups = groupDigits $ reverse s
+      groups = map reverse reversedGroups
+      digitLists = toDigitLists groups
+      digit3s = map toDigit3 digitLists
+   in zip digit3s illion
+
+groupDigits :: List a -> List (List a)
+groupDigits Nil = Nil
+groupDigits s =
+  let taken = take 3 s
+      remaining = drop 3 s
+   in taken :. groupDigits remaining
+
+toDigitLists :: List Chars -> List (List Digit)
+toDigitLists Nil = Nil
+toDigitLists (h :. t) = f $ traverse fromChar h
+  where
+    f Empty = Nil
+    f (Full digits) = digits :. toDigitLists t
+
+toDigit3 :: List Digit -> Digit3
+toDigit3 (a :. b :. c :. _) = D3 a b c
+toDigit3 (a :. b :. _) = D2 a b
+toDigit3 (a :. _) = D1 a
+toDigit3 Nil = error "You did something wrong"
+
+translate :: List (Digit3, Chars) -> Chars
+translate Nil = ""
+translate ((d3, "") :. t) = translate t ++ (showDigit3 d3 ++ " ")
+translate ((d3, thousandsString) :. t) = translate t ++ (showDigit3 d3 ++ " " ++ thousandsString ++ " ")
+
+-- "12345.678"
+-- ("12345", ".678")
+-- ["543", "21"]
+-- ["345", "12"]
+-- [[Three, Four, Five], [One, Two]]
+-- [D3 Three Four Five, D2 One Two]
+-- [(D3 Three Four Five, ""), (D2 One Two, "Thousand")]
